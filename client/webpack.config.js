5ce340a95ed6728eea19f70db2fcd27f080cf9c7
JimJamLee <li.james.chien@gmail.com>
2022-11-27 20:12:22 -0800

    Updated webpack config file for workbox plugins and CSS and Babel loaders

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,57 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Webpack plugin to generate html file and inject the bundles.
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "JATE"
+      }),
+      // Plugin to inject the custom service worker.
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js"
+      }),
+      // Plugin to create the manifest json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "An easy to use text editor!",
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      })
+
     ],
 
     module: {
+      // Rules for the CSS loader
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // Babel-loader to use ES6
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
